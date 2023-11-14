3d0653af2986195f2b20aeceb4c6eac7c34653aa
Pedro Fernandes <pedrovosvisk10@gmail.com>
2023-11-14 10:32:38 -0300

    Update main.js

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,10 +1,30 @@
-function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play();
+// function tocaSomPom() {
+//     document.querySelector('#som_tecla_pom').play();
+// }
+
+// function tocaSomClap() {
+//     document.querySelector('#som_tecla_clap').play();
+// }
+
+// document.querySelector('.tecla_pom').onclick = tocaSomPom;
+// document.querySelector('.tecla_clap').onclick = tocaSomClap;
+
+function tocaSons(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+
 }
 
-function tocaSomClap() {
-    document.querySelector('#som_tecla_clap').play();
-}
+const lista_de_teclas = document.querySelectorAll('.tecla');
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
-document.querySelector('.tecla_clap').onclick = tocaSomClap;
+let contador = 0;
+
+while (contador < lista_de_teclas.length) {
+    const tecla = lista_de_teclas[contador]
+    const instrumento = tecla.classList[1]
+    const idAudio = `#som_${instrumento}`
+    tecla.onclick = function () {
+        tocaSons(idAudio)
+    }
+    contador = contador + 1
+    // console.log(lista_de_teclas[contador])
+}
